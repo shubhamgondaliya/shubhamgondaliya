15f15c0cfa5f6430048c08ee8441977950350266
shubhamgondaliya <160833312+shubhamgondaliya@users.noreply.github.com>
2024-03-20 10:21:07 +0530

    Add files via upload

---
+++ sum of 2 no.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"enter a number:";
+	cin>>a;
+	cout<<"enter a number:";
+	cin>>b;
+	sum=a+b;
+	cout<< "\nThe sum of the Numbers is : " <<sum <<endl;
+	return 0;
+}
